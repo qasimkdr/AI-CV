601d905c618ae1b5bd48fada2773e1bd24315ceb
qasimkdr <kdrqasim@gmail.com>
2026-06-25 20:23:14 +0500

    replaced backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -8,7 +8,7 @@ import {
 const api =
   axios.create({
     baseURL:
-      "http://localhost:5000/api",
+      "https://ai-cv-qf0m.onrender.com",
     headers: {
       "Content-Type":
         "application/json",
